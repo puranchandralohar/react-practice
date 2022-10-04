a985b28178b35b8351697702c6f433b70b0c5d2c
puranchandralohar <clpuran95@gmail.com>
2022-10-04 11:40:44 +0530

    Style Added

--- registration-form/src/Components/Login.jsx
+++ registration-form/src/Components/Login.jsx
@@ -4,14 +4,16 @@ import './Login.css'
 export function Login(){
     return(
         <div className='login_container'>
+            
             <form className='login_form'>
+            <h1>Login</h1>
                 <label htmlFor="username">
                     <input type="text" name='uname' placeholder='Enter Username'/>
                 </label> 
                 <label htmlFor="password">
                     <input type="text" name='password' placeholder='Enter Password'/>
                 </label>
-                <button type='submit'>Login</button>
+                <button className='submit_btn' type='submit'>Login</button>
             </form>
 
         </div>
