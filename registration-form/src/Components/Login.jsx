51ba96d4d58cca3aae26b7e488b6d0db55f12f04
puranchandralohar <clpuran95@gmail.com>
2022-10-04 13:00:24 +0530

    Changes

--- registration-form/src/Components/Login.jsx
+++ registration-form/src/Components/Login.jsx
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import "./Login.css";
 
 export function Login() {
-const [data,setData] = useState({})
+const [data,setData] = useState({
+    username:"",
+    password:""
+})
+const [submit,setSubmit] = useState(false);
 
 const handleData = (e)=>{
    const name = e.target.name;
@@ -13,6 +17,10 @@ const handleData = (e)=>{
    })
 }
 
+const isSubmitted = (e)=>{
+    e.preventDefault()
+    setSubmit(true)
+}
 
 
 
@@ -21,14 +29,26 @@ const handleData = (e)=>{
 
   return (
     <div className="login_container">
-      <form className="login_form">
+      <form className="login_form" onSubmit={isSubmitted}>
         <h1>Login</h1>
         <label htmlFor="username">
-          <input type="text" name="username" value={data.username} onChange={handleData} placeholder="Enter Username" />
+          <input 
+          type="text" 
+          name="username" 
+          value={data.username} 
+          onChange={handleData} 
+          placeholder="Enter Username" />
         </label>
+
         <label htmlFor="password">
-          <input type="password" name="password" value={data.password} onChange={handleData}  placeholder="Enter Password" />
+          <input 
+          type="password" 
+          name="password" 
+          value={data.password} 
+          onChange={handleData}  
+          placeholder="Enter Password" />
         </label>
+
         <button className="submit_btn" type="submit">
           Login
         </button>
