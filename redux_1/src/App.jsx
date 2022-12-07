4ab00d92a008e2d8b52ec86ee66468c02345f7d9
puranchandralohar <clpuran95@gmail.com>
2022-12-07 23:26:10 +0530

    style using mui

--- redux_1/src/App.jsx
+++ redux_1/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Counter } from './Components/Counter/Counter'
 
+import './app.css'
+
 export const App = () => {
     return(
         <>
--- redux_1/src/Components/Counter/Counter.jsx
+++ redux_1/src/Components/Counter/Counter.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { incrementing, decrementing } from '../Features/CounterSlice';
 
+// import Button from '@mui/material'
+import Button from '@mui/material/Button';
+
 export const Counter = () => {
     const count = useSelector((state) => state.counter.value)
     const dispatch = useDispatch()
 
+
   return (
     <div>
         <h1>Counter</h1>
-        <div className="app">
-            <button onClick={()=>dispatch(incrementing())}>+</button>
-            <h1>{count}</h1>
-            <button onClick={()=>dispatch(decrementing())}>-</button>
+        <div className="app flex">
+            <Button variant="contained" onClick={()=>dispatch(incrementing())}>+</Button>
+            <span>{count}</span>
+            <Button variant= "outlined" onClick={()=>dispatch(decrementing())}>-</Button>
         </div>
     </div>
   )
