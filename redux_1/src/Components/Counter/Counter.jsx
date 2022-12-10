42b80272db40b5d321a4988e0a62e15ece422e0a
puranchandralohar <clpuran95@gmail.com>
2022-12-10 18:00:36 +0530

    Added

--- redux_1/src/Components/Counter/Counter.jsx
+++ redux_1/src/Components/Counter/Counter.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { incrementing, decrementing } from '../Features/CounterSlice';
 
-// import Button from '@mui/material'
+import { Typography, Stack } from '@mui/material';
 import Button from '@mui/material/Button';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import { Box } from '@mui/system';
 
 export const Counter = () => {
     const count = useSelector((state) => state.counter.value)
@@ -11,13 +14,13 @@ export const Counter = () => {
 
 
   return (
-    <div>
-        <h1>Counter</h1>
-        <div className="app flex">
-            <Button variant="contained" onClick={()=>dispatch(incrementing())}>+</Button>
-            <span>{count}</span>
-            <Button variant= "outlined" onClick={()=>dispatch(decrementing())}>-</Button>
-        </div>
-    </div>
+    <Box>
+        <Typography variant="h3" component="h1">Counter</Typography>
+        <Stack direction="row">
+            <Button variant="contained" onClick={()=>dispatch(incrementing())}><AddCircleIcon /></Button>
+            <Typography variant='h4'>{count}</Typography>
+            <Button variant= "outlined" onClick={()=>dispatch(decrementing())}><RemoveCircleIcon/></Button>
+        </Stack >
+    </Box>
   )
 }
